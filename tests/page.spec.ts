9900e29282d512347cad672803ce2903754f4312
Jan Borchers <humancatfood@users.noreply.github.com>
2023-05-10 18:01:28 +0100

    delete useless snapshot test

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -4,5 +4,4 @@ test('index page', async ({ page }) => {
 	await page.goto('/');
 	await expect(page).toHaveTitle('HumanCatfood');
 	await expect(page.getByRole('heading', { name: 'Hi!' })).toBeVisible();
-	await expect(page).toHaveScreenshot('index.png');
 });
