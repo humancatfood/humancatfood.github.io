5a7c5374002c78bb9d6599589dcbd42d2198fabc
Jan Borchers <humancatfood@users.noreply.github.com>
2023-05-10 22:34:42 +0100

    one more try with the snapshots

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -4,4 +4,7 @@ test('index page', async ({ page }) => {
 	await page.goto('/');
 	await expect(page).toHaveTitle('HumanCatfood');
 	await expect(page.getByRole('heading', { name: 'Hi!' })).toBeVisible();
+	await expect(page).toHaveScreenshot('index.png', {
+		maxDiffPixelRatio: 0.1
+	});
 });
